f96254b07163d2e32cf6f1cca92f6dc810956e8c
SaryJoval <saraarenasv@gmail.com>
2017-11-15 12:34:05 -0300

    primer commit

---
+++ src/main/java/cl/accenture/curso_java/sistema_de_reserva/dao/UsuarioDAO.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package cl.accenture.curso_java.sistema_de_reserva.dao;
+
+/**
+ * @author Administrador
+ *
+ */
+public class UsuarioDAO {
+
+}
---
+++ src/main/java/cl/accenture/curso_java/sistema_de_reserva/modelo/Usuario.java
@@ -0,0 +1,5 @@
+package cl.accenture.curso_java.sistema_de_reserva.modelo;
+
+public class Usuario {
+
+}
