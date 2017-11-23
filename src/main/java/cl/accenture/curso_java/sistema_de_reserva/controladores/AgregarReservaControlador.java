b4a65da4c7dd50229e2c75ef7fe869ae05bbcbea
Luis Torres <torrescereno@gmail.com>
2017-11-23 01:30:41 -0300

    Reserva de horas

--- src/main/java/cl/accenture/curso_java/sistema_de_reserva/controladores/AgregarReservaControlador.java
+++ src/main/java/cl/accenture/curso_java/sistema_de_reserva/controladores/AgregarReservaControlador.java
@@ -1,22 +1,24 @@
 package cl.accenture.curso_java.sistema_de_reserva.controladores;
 
 import java.io.Serializable;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
+import cl.accenture.curso_java.sistema_de_reserva.dao.ConfiguracionDAO;
 import cl.accenture.curso_java.sistema_de_reserva.dao.ReservaDAO;
-import cl.accenture.curso_java.sistema_de_reserva.dao.ReservaHoraDAO;
 import cl.accenture.curso_java.sistema_de_reserva.dao.SucursalDAO;
+import cl.accenture.curso_java.sistema_de_reserva.modelo.Configuracion;
 import cl.accenture.curso_java.sistema_de_reserva.modelo.Reserva;
-import cl.accenture.curso_java.sistema_de_reserva.modelo.ReservaHora;
 import cl.accenture.curso_java.sistema_de_reserva.modelo.Sucursal;
 import cl.accenture.curso_java.sistema_de_reserva.modelo.Usuario;
 
@@ -32,15 +34,22 @@ public class AgregarReservaControlador implements Serializable {
 	private String mensaje;
 	private String servicio;
 	private String nombre;
+	private String horaInicio;
+	private String horaFin;
+	private String bloque;
+
+	private Calendar hora;
+
 	private Date fechaReserva;
-	private String hora;
-	private String date;
+
 	private List<Sucursal> sucursales;
-	private List<ReservaHora> reservasHoras;
+	private List<Calendar> horas;
+	private List<Configuracion> configuraciones;
 
 	public AgregarReservaControlador() {
 		obtenerSucursal();
-		//obtenerReservasHoras();
+		//obtenerHorasDisponibles();
+
 	}
 
 	public void obtenerSucursal() {
@@ -54,28 +63,62 @@ public class AgregarReservaControlador implements Serializable {
 		}
 	}
 
-	public void obtenerReservasHoras() {
+	public void obtenerHorasDisponibles() {
 
 		try {
-			Calendar cal = GregorianCalendar.getInstance();
-			cal.setTime( this.fechaReserva );
-			cal.add(Calendar.DAY_OF_MONTH, 1);
+
+			// Calendar cal = GregorianCalendar.getInstance();
+			// cal.setTime( this.fechaReserva );
+			// cal.add(Calendar.DAY_OF_MONTH, 1);
+			//
+			// String horaInicio = "10:00:00";
+			// String horaFin = "18:00:00";
+			// String bloque = "30";
+
+			this.configuraciones = ConfiguracionDAO.obtenerConfiguraciones();
+
+			this.bloque = this.configuraciones.get(0).getValor();
+			this.horaFin = this.configuraciones.get(1).getValor();
+			this.horaInicio = this.configuraciones.get(2).getValor();
 			
-			String horaInicio = "10:00:00";
-			String horaFin 	  = "18:00:00";
-			String bloque     = "30";
+//			System.out.println(bloque);
+//			System.out.println(horaFin);
+//			System.out.println(horaInicio);
+
 			
+			DateFormat horaformar = new SimpleDateFormat("hh:mm:ss");
+
+			Date bloqueT = horaformar.parse(this.bloque);
+			Date horaFinT = horaformar.parse(this.horaFin);
+			Date horaInicioT = horaformar.parse(this.horaInicio);
+			
+			System.out.println(horaFinT.getTime());
+			System.out.println(horaInicioT.getTime());
+
 			
 
-			SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
-			this.date = formato.format(cal.getTime());
-			this.reservasHoras = ReservaHoraDAO.obtenerHorasDisponibles(this.date);
+			Calendar cal = Calendar.getInstance();
+
+			while (cal.getTime().equals(horaFinT)) {
+
+				cal.setTime(horaInicioT);
+				cal.add(Calendar.MINUTE, 30);
+
+				this.hora.setTime(cal.getTime());
+				this.horas.add(this.hora);
+
+			}
+
+			// SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+			// this.date = formato.format(cal.getTime());
+			// this.reservasHoras = ReservaHoraDAO.obtenerHorasDisponibles(this.date);
 			this.mensaje = "";
 			//
-			System.out.println(this.date);
+			// System.out.println(this.date);
 		} catch (Exception e) {
 			this.mensaje = "Lo sentimos, Ocurrio un error al obtener las Horas";
-			this.reservasHoras = new ArrayList<ReservaHora>();
+			// this.reservasHoras = new ArrayList<ReservaHora>();
+			this.configuraciones = new ArrayList<Configuracion>();
 		}
 	}
 
@@ -89,7 +132,6 @@ public class AgregarReservaControlador implements Serializable {
 		reserva.setServicio(this.servicio);
 		reserva.setSucursal(this.nombre);
 		reserva.setFechaReserva(this.fechaReserva);
-		reserva.setHora(this.hora);
 
 		try {
 			ReservaDAO.agregarReserva(reserva, sucursal, usuario);
@@ -104,9 +146,6 @@ public class AgregarReservaControlador implements Serializable {
 
 	public void mostrar() {
 
-		System.out.println(this.fechaReserva);
-		System.out.println(this.servicio);
-		System.out.println(this.nombre);
 	}
 
 	public String getMensaje() {
@@ -141,14 +180,6 @@ public class AgregarReservaControlador implements Serializable {
 		this.fechaReserva = fechaReserva;
 	}
 
-	public String getHora() {
-		return hora;
-	}
-
-	public void setHora(String hora) {
-		this.hora = hora;
-	}
-
 	public List<Sucursal> getSucursales() {
 		return sucursales;
 	}
@@ -161,12 +192,52 @@ public class AgregarReservaControlador implements Serializable {
 		return serialVersionUID;
 	}
 
-	public List<ReservaHora> getReservasHoras() {
-		return reservasHoras;
+	public List<Calendar> getHoras() {
+		return horas;
 	}
 
-	public void setReservasHoras(List<ReservaHora> reservasHoras) {
-		this.reservasHoras = reservasHoras;
+	public void setHoras(List<Calendar> horas) {
+		this.horas = horas;
+	}
+
+	public Calendar getHora() {
+		return hora;
+	}
+
+	public void setHora(Calendar hora) {
+		this.hora = hora;
+	}
+
+	public List<Configuracion> getConfiguraciones() {
+		return configuraciones;
+	}
+
+	public void setConfiguraciones(List<Configuracion> configuraciones) {
+		this.configuraciones = configuraciones;
+	}
+
+	public String getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(String horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public String getHoraFin() {
+		return horaFin;
+	}
+
+	public void setHoraFin(String horaFin) {
+		this.horaFin = horaFin;
+	}
+
+	public String getBloque() {
+		return bloque;
+	}
+
+	public void setBloque(String bloque) {
+		this.bloque = bloque;
 	}
 
 }
